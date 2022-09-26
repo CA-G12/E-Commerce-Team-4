fffb6b579726a7a72061dc6cffb0199906b0e1d9
abdou059 <abdoumonuir9@gmail.com>
2022-09-26 09:53:44 +0300

    check email query

---
+++ server/database/queries/auth/checkEmail.js
@@ -0,0 +1,13 @@
+const connection = require('../../config/connection');
+
+const checkEmail = (email) => {
+    const sql = {
+        text: 'select id from users where email = $1',
+        values: [email],
+    };
+    return connection.query(sql);
+};
+
+module.exports = {
+    checkEmail,
+};
