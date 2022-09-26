77fdb51276854103931849ac68460d83ddfbcae0
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-26 13:20:11 +0300

    Added the get cart product controllers close #14

--- server/controllers/cart/getCartProduct.js
+++ server/controllers/cart/getCartProduct.js
@@ -1,7 +1,16 @@
-const getCartProduct = () => {
-  // ? Do something.
+const { getCartProductsForUserQuery } = require('../../database/queries');
+const CustomizedError = require('../../errors/customError');
+
+const getCartProducts = (req, res) => {
+  const { id } = req.params;
+
+  getCartProductsForUserQuery(id)
+    .then((data) => res.json({ products: data.rows }))
+    .catch((err) => {
+      throw new CustomizedError(500, `Error: ${err}`);
+    });
 };
 
 module.exports = {
-  getCartProduct,
+  getCartProducts,
 };
