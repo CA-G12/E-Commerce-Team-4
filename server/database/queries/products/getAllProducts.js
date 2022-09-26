5f2b38dc1c85ea23a0d876deebcd1abf9ce33e04
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-09-26 12:23:02 +0300

    create get products  queries (by price , category) relate 23 ,24

--- server/database/queries/products/getAllProducts.js
+++ server/database/queries/products/getAllProducts.js
@@ -1,13 +1,48 @@
 const connection = require('../../config/connection');
 
-const getAllProducts = () => {
+const getAllProductsQuery = () => {
   const sql = {
-    text: '',
+    text: 'select * from products Limit 2;',
     values: [],
   };
-  return connection.query(sql);
+  return connection.query(sql.text);
 };
 
+const getAllProductsDescQuery = () => {
+  const sql = {
+    text: 'select * from products ORDER BY price DESC',
+    values: [],
+  };
+  return connection.query(sql.text);
+};
+
+
+const getAllProductsAscQuery = () => {
+  const sql = {
+    text: 'select * from products ORDER BY price ASC',
+    values: [],
+  };
+  return connection.query(sql.text);
+};
+
+const getProductsByCategoryQuery = (category) => {
+  const sql = {
+    text: `select * from products where category=$1`,
+    values: [category],
+  };
+  return connection.query(sql.text, sql.values);
+};
+
+
+const showMoreProductsQuery = () => {
+  const sql = {
+    text: 'select * from products OFFSET 2 ROWS FETCH NEXT 2 ROWS ONLY;',
+    values: [],
+  };
+  return connection.query(sql.text);
+};
+
+
 module.exports = {
-  getAllProducts,
+  getAllProductsQuery, getAllProductsDescQuery, getAllProductsAscQuery, getProductsByCategoryQuery, showMoreProductsQuery
 };
