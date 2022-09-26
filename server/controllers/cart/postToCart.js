e7695670f6468d6c99bfd4eaa2b06fe2b3f0fb81
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-26 13:20:39 +0300

    Added the post to cart controllers close #14

--- server/controllers/cart/postToCart.js
+++ server/controllers/cart/postToCart.js
@@ -1,5 +1,14 @@
-const postToCart = () => {
-  // ? Do something.
+const { postToCartQuery } = require('../../database/queries');
+const CustomizedError = require('../../errors/customError');
+
+const postToCart = (req, res) => {
+  const { userId, productId } = req.body;
+
+  postToCartQuery(userId, productId)
+    .then((data) => res.json({ message: `${data.rowCount} rows were added!!` }))
+    .catch((err) => {
+      throw new CustomizedError(500, `Error: ${err}`);
+    });
 };
 
 module.exports = {
