0f7c6231d5818ecb02b4bf96ab87e95e6a61a3dc
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-27 23:31:51 +0300

    Added some of the login request functionality close #65

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,15 +1,34 @@
 import { useState } from 'react';
+import axios from 'axios';
 import './Login.css';
 
 export default function Login() {
-  const [loginData, setLoginData] = useState({
-    email: 'hello@hello.com',
-    password: 'root',
-  });
+  const [loginData, setLoginData] = useState({});
+  const [isLogged, setIsLogged] = useState(false);
+  const [error, setError] = useState('');
 
   const handleInput = (e) => {
     setLoginData((prevData) => ({ ...prevData, [e.target.name]: e.target.value }));
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = loginData;
+    axios.post('/api/v1/login', {
+      email,
+      password,
+    })
+      .then((data) => {
+        console.log(data);
+        setIsLogged(true);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+
+  if (error) return <h1 className="not-logged">Wrong password or email</h1>;
+  if (isLogged || loginData.msg) return <h1 className='success'>Logged in successfully</h1>
 
   return (
     <section className="login-outer-cont">
@@ -45,7 +64,11 @@ export default function Login() {
             />
           </label>
           <br />
-          <button className="submit" type="submit">Submit</button>
+          <button
+            className="submit"
+            type="submit"
+            onClick={handleSubmit}
+          >Submit</button>
         </form>
       </section>
     </section>
