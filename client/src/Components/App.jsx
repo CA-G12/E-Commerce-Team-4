da045a21d3bc83ba56bae19330c2c1ee52d7de3d
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-27 11:05:11 +0300

    format

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,16 +1,16 @@
-import './App.css';
-import Header from './LandingPage/Header/Header';
-import Footer from './LandingPage/Footer/Footer';
-import LandingPage from './LandingPage/LandingPage';
+import './App.css'
+import Header from './LandingPage/Header/Header'
+import Footer from './LandingPage/Footer/Footer'
+import LandingPage from './LandingPage/LandingPage'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header />z
       <LandingPage />
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
