3a4162710640c30b55ad43773ea3b5bca998c897
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-27 14:09:50 +0300

    added onchange attribute to input element. relates #62

--- client/src/Components/LandingPage/Products/Filter/FilterPrice.jsx
+++ client/src/Components/LandingPage/Products/Filter/FilterPrice.jsx
@@ -1,14 +1,30 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+
 function FilterPrice() {
+  const handle = () => 'dadada'
   return (
     <div className="filter-price-container">
       <h4>Range Price</h4>
       <label htmlFor="points">Max Price:</label>
-      <input type="range" name="points" min="0" max="10" value={7} />
+      <input
+        type="range"
+        name="points"
+        min="0"
+        max="10"
+        value={7}
+        onChange={handle}
+      />
       <label htmlFor="points">Min Price:</label>
-      <input type="range" name="points" min="0" max="10" value={0} />
+      <input
+        type="range"
+        name="points"
+        min="0"
+        max="10"
+        value={0}
+        onChange={handle}
+      />
     </div>
-  );
+  )
 }
 
-export default FilterPrice;
+export default FilterPrice
