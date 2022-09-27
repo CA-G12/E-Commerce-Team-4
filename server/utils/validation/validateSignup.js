b67c329b9fc37e4498f0cdae3f8e8a50dc3dad5b
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-27 23:35:52 +0300

    Edited the validation criterias close #65

--- server/utils/validation/validateSignup.js
+++ server/utils/validation/validateSignup.js
@@ -1,19 +1,14 @@
-require('dotenv').config();
-const joi = require('joi');
+const Joi = require('joi');
 
-const signUpValidationSchema = joi.object({
-  name: joi.string().required(),
-  username: joi.string().alphanum().min(3).max(30).required(),
-  email: joi
-    .string()
-    .email()
-    .required()
-    .pattern(/^[a-zA-z0-9]?.*@[a-zA-z0-9]{1,}.[a-zA-Z]{1,}$/),
-  password: joi.string().alphanum().min(7).max(30),
-  confirmPassword: joi.ref('password'),
-  country: joi.string().required(),
-  address: joi.string().required(),
-  imgUrl: joi
+const signUpValidationSchema = Joi.object({
+  name: Joi.string().required(),
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().email().pattern(/^[a-zA-z0-9]?.*@[a-zA-z0-9]{1,}.[a-zA-Z]{1,}$/).required(),
+  password: Joi.string().pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$/).required(),
+  confirmPassword: Joi.ref('password'),
+  country: Joi.string().required(),
+  address: Joi.string().required(),
+  imgUrl: Joi
     .string()
     .empty('')
     .default(
