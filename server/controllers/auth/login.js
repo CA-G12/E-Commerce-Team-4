aeda14a0b47af470336501249f4ac98cab3b65a2
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-27 23:33:00 +0300

    Edited the loginSchema functionality and add checkEmail query close #65

--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -1,29 +1,34 @@
-const { getPassword } = require('../../database/queries');
-const schemaLogin = require('../../utils/validation');
+const { checkEmail, getPassword } = require('../../database/queries');
+const { schemaLogin } = require('../../utils');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const CustomizedError = require('../../errors/customError');
 
 const controllerLogin = (req, res) => {
   let userId;
-  schemaLogin
-    .validateAsync(req.body)
-    .then((data) => getPassword(data.email))
+  schemaLogin.validateAsync(req.body)
+    .then((data) => checkEmail(data.email))
+    .then((emailData) => {
+      if (emailData.rowCount === 0) {
+        throw new CustomizedError(400, 'Email does not exist');
+      }
+      return getPassword(data.email);
+    })
     .then((res) => {
-      if (res.rowCount == 0) {
-        throw new CustomizedError(500, 'wrong email or password');
+      if (res.rowCount === 0) {
+        throw new CustomizedError(400, 'wrong email or password');
       }
       const { id, password } = res.rows[0];
       userId = id;
       return bcrypt.compare(req.body.password, password);
     })
     .then((data) => {
-      if (data == false) {
-        throw new CustomizedError(500, 'wrong email or password');
+      if (data === false) {
+        throw new CustomizedError(400, 'wrong email or password');
       } else {
-        jwt.sign({ userId }, process.env.SECRET_KEY, (err, token) => {
+        jwt.sign({ userId, password: req.body.password }, process.env.SECRET_KEY, (err, token) => {
           if (err) {
-            throw new Error('server error');
+            throw new CustomizedError(500, 'server error');
           } else {
             res.cookie('token', token).json({
               msg: 'login true',
