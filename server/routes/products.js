0be033ccfaf5b2202ee44b2852bac1230a9a7f67
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-28 12:30:59 +0300

    added product details endpoint router to get detailes of product from database by id. relates #72

--- server/routes/products.js
+++ server/routes/products.js
@@ -5,12 +5,14 @@ const {
   getProductsByPriceAsc,
   getProductsByCategory,
   showMoreProducts,
-} = require('../controllers/index');
+  productDetails,
+} = require('../controllers');
 
 router.get('/products', getProducts);
 router.get('/productsByPriceDesc', getProductsByPriceDesc);
 router.get('/productsByPriceAsc', getProductsByPriceAsc);
 router.get('/products/:category', getProductsByCategory);
 router.get('/showMore/?:number', showMoreProducts);
+router.get('/product/:id', productDetails);
 
 module.exports = router;
