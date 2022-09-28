152450e5655a240b093de8acb4f1fe3a6f580e66
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-28 23:59:33 +0300

    added check user login functionailty thats check if the user is loged in thats will change header structure. relates #79

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,18 +1,35 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
+  const [status, setStatus] = useState('Unauthenticated');
+
+  useEffect(() => {
+    axios.get('/api/v1/check-login').then(({ data }) => setStatus(data.msg));
+  }, []);
+
   return (
     <header>
       <nav>
         <p>SOQE</p>
-        <div>
-          <ion-icon name="search-outline" />
-          <ion-icon name="person-outline" />
-          <ion-icon name="cart-outline" />
-        </div>
+        {status === 'authenticated' ? (
+          <div>
+            <ion-icon name="search-outline" />
+            <ion-icon name="person-outline" />
+            <Link to="cart">
+              <ion-icon name="cart-outline" />
+            </Link>
+          </div>
+        ) : (
+          <div className="btn">
+            <Link to="/login">login</Link>
+            <Link to="/signup">signup</Link>
+          </div>
+        )}
       </nav>
     </header>
-
   );
 }
 
