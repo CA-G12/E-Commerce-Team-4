4f35a656f40f1c35bdfe0093571b149b4389b2e0
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-28 22:32:18 +0300

    Added the footer and added extra styles close #76

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,11 +1,14 @@
 import Products from './Products/Products';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import './Cart.css';
 
 function Cart() {
   return (
     <section className="products-container">
       <Header />
       <Products />
+      <Footer />
     </section>
   );
 }
